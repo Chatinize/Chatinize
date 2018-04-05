1ef14aca0f9c4f719825a60cd00c8bfde5f03c9f
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-04-05 09:34:23 -0600

    adding passport to app.js

--- server/app.js
+++ server/app.js
@@ -3,9 +3,67 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const queries = require("./queries");
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const GitStrategy = require('passport-github').Strategy;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'bquyqueajhbd',
+  resave: true,
+  saveUninitialized: true,
+  store: new FileStore({ path: '/tmp/sessions' });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new GitStrategy({
+  clientID: process.env.GIT_PUBLIC,
+  clientSecret: process.env.GIT_SECRET,
+  callbackURL: "http://localhost:3000/auth/github/callback"
+},
+  function (clientID, clientSecret, profile, done) {
+    queries.readUsers('users', profile.id).then(user => {
+      let newUser = JSON.parse(JSON.stringify(user))
+      if (!newUser) {
+        newUser = { git_id: profile.id, email: profile.emails[0].value, username: profile.displayName, avatar: profile.photos[0].value, github: profile.profileUrl }
+        queries.create('users', newUser).then(user => {
+          let userData = JSON.parse(JSON.stringify(user))
+          response.json(userData);
+        })
+      }
+      done(null, newUser)
+    })
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  console.log('serializeUser: ' + user.id)
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  console.log('deserializeUser: ' + user.id)
+  let user = users.find(u => u.id === id)
+  if (!user) { done(new Error('User not found! ' + id)) }
+  done(null, user)
+});
+
+app.get('/', express.static('./public/index.html'))
+
+app.get('/auth', (req, res, next) => {
+  console.log('session:', req.session)
+  res.send({ user: req.user })
+})
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: 'http://localhost:8080/home/#/home',
+    failureRedirect: 'http://localhost:3000/auth/github[0]'
+  }));
+
+app.get('/auth/github', passport.authenticate('github'));
 
 app.get("/users", (request, response) => {
   queries
